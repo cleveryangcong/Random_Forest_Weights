9b4eb36d61a36d3507655c460fa1ac94868d60d9
Delong <delongchen10062002@gmail.com>
2023-08-11 09:35:23 +0200

    Make modifications to calc_weights_energy for better memory management

--- src_rf/datasets/calc_weights_energy.py
+++ src_rf/datasets/calc_weights_energy.py
@@ -8,6 +8,7 @@ Save calculated weights, in correct order
 # Basics
 import pandas as pd
 import numpy as np
+import gc  # garbage collector
 
 # Model
 from sklearn.ensemble import RandomForestRegressor
@@ -27,34 +28,34 @@ from src_rf.methods.calc_weights import *
 from src_rf.methods.calc_dist import *
 
 
-
 def compute_rf_weights(args):
     rf, X_train, batch, bootstrap, max_samples = args
-    return calc_weights_rf(rf, X_train, batch, bootstrap, max_samples)     
-        
+    return calc_weights_rf(rf, X_train, batch, bootstrap, max_samples)
+
+
 if __name__ == "__main__":
     # 1. Load Data:
     df = pd.read_csv("/home/dchen/Random_Forest_Weights/src_rf/data/energy_data_hourly.csv", index_col="datetime")
     df.index = pd.to_datetime(df.index)
-    
+
     # 2. Train Test Split:
-    X = df.drop('total_energy_usage', axis = 1).values
-    y = df['total_energy_usage'].values    
-        
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state = 42)   
-    
+    X = df.drop('total_energy_usage', axis=1).values
+    y = df['total_energy_usage'].values
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=42)
+
     # 3. Random Forest:
-        # 3.1 Parameters for Weight_Calculation:
+    # 3.1 Parameters for Weight_Calculation:
     bootstrap = False
     max_samples = 0.8
-        # 3.2 Parameters for RF
-    
-        # 3.3 Model Training
+    # 3.2 Parameters for RF
+
+    # 3.3 Model Training
     rf = RandomForestRegressor(
-    bootstrap=bootstrap, max_samples = max_samples, verbose=0, n_jobs=-1
-)
-    rf.fit(X_train, y_train)    
-    
+        bootstrap=bootstrap, max_samples=max_samples, verbose=0, n_jobs=-1
+    )
+    rf.fit(X_train, y_train)
+
     # 4. Parallel Processing:
     num_samples = X_test.shape[0]
     batch_size = 50
@@ -62,23 +63,26 @@ if __name__ == "__main__":
     # Define the number of processes you want to run in parallel
     max_workers = 2
     # Split the data into batches
-    batches = [(X_test[i:i+batch_size], i, i+batch_size) for i in range(0, num_samples, batch_size)]
+    batches = [(X_test[i:i + batch_size], i, i + batch_size) for i in range(0, num_samples, batch_size)]
 
     # Use ProcessPoolExecutor to parallelize computation
-    with ProcessPoolExecutor(max_workers = max_workers) as executor:
-        args = [(rf, X_train, batch, bootstrap, max_samples) for batch, _, _ in batches]
-        results = list(executor.map(compute_rf_weights, args))
+    with ProcessPoolExecutor(max_workers=2) as executor:  # Set max_workers to 2
+        futures = [executor.submit(compute_rf_weights, (rf, X_train, batch, bootstrap, max_samples)) for batch, _, _ in batches]
 
-    # Now, save the results batch-wise to ensure order
-    for rf_weights in results:
-        # Load existing data, concatenate and save back
-        if os.path.exists(file_path):
-            existing_data = np.load(file_path)
-            combined_data = np.concatenate([existing_data, rf_weights], axis=0)
-        else:
-            combined_data = rf_weights
+        for future in futures:  # Iterate over futures in the order they were submitted
+            rf_weights = future.result()
 
-        np.save(file_path, combined_data)
+            # Save rf_weights immediately to disk
+            if os.path.exists(file_path):
+                existing_data_shape = np.load(file_path, mmap_mode='r').shape
+                new_shape = (existing_data_shape[0] + rf_weights.shape[0],) + rf_weights.shape[1:]
+                combined_data = np.memmap(file_path, dtype=rf_weights.dtype, mode='r+', shape=new_shape)
+                combined_data[-rf_weights.shape[0]:] = rf_weights
+            else:
+                np.save(file_path, rf_weights)
+
+            del rf_weights  # Explicitly delete to help with memory management
+            gc.collect()  # Call garbage collector to free up memory
 
         
         
